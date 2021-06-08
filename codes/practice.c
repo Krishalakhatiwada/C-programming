e773bbcc5c071fd365185532162d857e483fd9ef
Krishala Khatiwada <krishalakhatiwoda1999@gmail.com>
2021-06-08 20:57:05 +0545

    task done

--- codes/practice.c
+++ codes/practice.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
     /*print ASCII value of a character*/
-    char H;
+    char c;
     printf("enter a character:\n");
-    scanf("%c",&H);
-    printf("%d",H);
+    scanf("%c",&c);
+    printf("%d",c);
 
 }
