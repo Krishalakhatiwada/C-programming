7ca0e1f0dfddae5f26d2adece7f8e2f484e1917f
Krishala Khatiwada <krishalakhatiwoda1999@gmail.com>
2021-06-08 18:39:17 +0545

    task completed

---
+++ codes/implementOperators.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a=0,b=0,c=1,d=0;
+    float e=0,f=0,g=0,h=0;
+    printf("Enter the number:");
+    printf("++a=%d\n",++a);
+printf("++b=%d\n",++b);
+printf("++c=%d\n",++c);
+printf("++d=%d\n",++d);
+printf("++e=%f\n",++e);
+printf("++f=%10f\n",++f);
+printf("++g=%2f\n",++g);
+printf("++h=%f\n",++h);
+
+}
